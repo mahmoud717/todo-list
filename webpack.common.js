476843749ad368973b0564301d2ac2ca1ab961b6
Mahmoud Mohammad <mahmoudmohammad717@gmail.com>
2020-12-08 10:18:36 +0200

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/app/index.js',
+    vendor: './src/app/vendor.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs',
+          },
+        },
+      },
+    ],
+  },
+};
