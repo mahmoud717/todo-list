0ff083b755eac5b7187f13d896fc50586a27d084
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-12-08 11:49:57 +0200

    finished structuring html and css

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,21 @@
 import '../assets/style/main.scss';
+
+class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+
+  switchPriority() {
+    console.log('hello');
+  }
+
+  done() {
+
+  }
+}
+
+const firstTodo = new Todo('asd', 'asd', 'ad', 'asd');
+firstTodo.switchPriority();
